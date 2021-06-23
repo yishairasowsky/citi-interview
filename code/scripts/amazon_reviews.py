7901cfaa8b6b6a5add011d4cc7ecf6861a255777
yishairasowsky <yishairasowsky@gmail.com>
2021-06-23 23:50:08 +0300

    expand def of loading amazon data in module

--- code/scripts/amazon_reviews.py
+++ code/scripts/amazon_reviews.py
@@ -1,25 +1,48 @@
 """
-*****************
 amazon_reviews.py
-*****************
+
 The purpose of this Python module is 
 to take data downloaded from one of the categories at
 http://deepyeti.ucsd.edu/jianmo/amazon/index.html,
-and eventually output a shuffled dataset required for BERT classification. 
+and eventually output a shuffled dataset 
+required for BERT classification. 
 """
 import pandas as pd
 
-def load_data(product_category='Appliances', random_state=42):
+def load_data(product_category='Appliances', row_limit=None, random_state=42):
     """
     Returns a dataframe containing Amazon reviews.
     Args:
-    - product_category (str) --> type of item purchased (default 'Appliances')
-    - random_state (int) --> seed for repeating experiments (default 42)
+    - product_category (str): type of item purchased
+    - random_state (int): seed for repeating experiments
+    - row_limit (int): limit on number of rows allowed
     """
-    file_name = f'data\\amazon_reviews\\{product_category}.json.gz'
-    raw_df = pd.read_json(file_name,compression='infer',lines=True)
-    shuffled_df = raw_df.sample(frac=1,random_state=random_state)
-    return shuffled_df
+    file_name = f'data\\amazon_reviews\\{product_category}.json.gz' 
+    df = pd.read_json(file_name,compression='infer',lines=True) 
+
+    df["text"] = df["summary"] + ' ' + df["reviewText"]
+    df["text"] = df["text"].str.lower()
+    
+    # df["overall"] = df['overall'].astype(str)
+
+    possible_labels = df.overall.unique()
+
+    label_dict = {possible_label:possible_label - 1 for possible_label in possible_labels}
+    # for possible_label in possible_labels:
+    #     label_dict[possible_label] = int(possible_label) - 1
+
+    df['label'] = df.overall.replace(label_dict)
+
+    cols = ['text','label']
+    df = df[cols]
+    df = df.dropna(subset=['label', 'text'])
+    
+    df = df.sample(frac=1,random_state=random_state) # shuffle
+
+    if row_limit:
+        df = df.iloc[:row_limit,:]
+
+    return df
 
 if __name__ == '__main__':
     df = load_data()

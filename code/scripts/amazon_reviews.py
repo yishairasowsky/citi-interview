dc3b4a076ed1af7f3a745713db71f4225bf9734e
yishairasowsky <yishairasowsky@gmail.com>
2021-06-24 00:00:14 +0300

    fix path to amazon reviews

--- code/scripts/amazon_reviews.py
+++ code/scripts/amazon_reviews.py
@@ -7,6 +7,7 @@ http://deepyeti.ucsd.edu/jianmo/amazon/index.html,
 and eventually output a shuffled dataset 
 required for BERT classification. 
 """
+import os
 import pandas as pd
 
 def load_data(product_category='Appliances', row_limit=None, random_state=42):
@@ -17,7 +18,8 @@ def load_data(product_category='Appliances', row_limit=None, random_state=42):
     - random_state (int): seed for repeating experiments
     - row_limit (int): limit on number of rows allowed
     """
-    file_name = f'data\\amazon_reviews\\{product_category}.json.gz' 
+    # file_name = f'data\\amazon_reviews\\{product_category}.json.gz' 
+    file_name = os.path.join(r'data/amazon_reviews',f'{product_category}.json.gz')
     df = pd.read_json(file_name,compression='infer',lines=True) 
 
     df["text"] = df["summary"] + ' ' + df["reviewText"]

8899e4cfa116ea1420425f4bd94ad28e3cb2932a
yishairasowsky <yishairasowsky@gmail.com>
2021-06-24 17:23:49 +0000

    add tokenization and tensor generation to the load data function

--- code/scripts/amazon_reviews.py
+++ code/scripts/amazon_reviews.py
@@ -10,7 +10,10 @@ https://github.com/naveenjafer/BERT_Amazon_Reviews/blob/master/main.py
 """
 import os
 import pandas as pd
+import torch
 
+from transformers import BertTokenizer
+from torch.utils.data import TensorDataset
 from sklearn.model_selection import train_test_split
 
 class DataManager:
@@ -50,7 +53,54 @@ class DataManager:
         # select relevant input features
         self.df["text"] = self.df["summary"] + ' ' + self.df["reviewText"]
         self.df["text"] = self.df["text"].str.lower()
-        
+
+    def train_val_split(self):
+        # split samples into training versus validation
+        X_train, X_val, _, _ = train_test_split(self.df.index.values, 
+                                                        self.df.label.values, 
+                                                        test_size=0.15, 
+                                                        random_state=42, 
+                                                        stratify=self.df.label.values)
+        # label data type in column
+        self.df['data_type'] = ['not_set']*self.df.shape[0] 
+        self.df.loc[X_train, 'data_type'] = 'train'
+        self.df.loc[X_val, 'data_type'] = 'val'
+
+    def tokenize(self):
+
+        tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', 
+                                            do_lower_case=True)
+
+        self.encoded_data_train = tokenizer.batch_encode_plus(
+            self.df[self.df.data_type=='train'].text.values, 
+            add_special_tokens=True, 
+            return_attention_mask=True, 
+            pad_to_max_length=True, 
+            max_length=256, 
+            return_tensors='pt'
+        )
+
+        self.encoded_data_val = tokenizer.batch_encode_plus(
+            self.df[self.df.data_type=='val'].text.values, 
+            add_special_tokens=True, 
+            return_attention_mask=True, 
+            pad_to_max_length=True, 
+            max_length=256, 
+            return_tensors='pt'
+        )
+
+    def generate_tensors(self):
+        input_ids_train = self.encoded_data_train['input_ids']
+        attention_masks_train = self.encoded_data_train['attention_mask']
+        labels_train = torch.tensor(self.df[self.df.data_type=='train'].label.values)
+
+        input_ids_val = self.encoded_data_val['input_ids']
+        attention_masks_val = self.encoded_data_val['attention_mask']
+        labels_val = torch.tensor(self.df[self.df.data_type=='val'].label.values)
+
+        dataset_train = TensorDataset(input_ids_train, attention_masks_train, labels_train)
+        dataset_val = TensorDataset(input_ids_val, attention_masks_val, labels_val)
+
     def load_data(self,row_limit=None,product_category='Appliances'):
         """
         Get shuffled datasets ready for BERT classification. 
@@ -75,57 +125,14 @@ class DataManager:
         # prepare for classification
         self.shuffle_rows()
         self.limit_rows(row_limit)
+        self.train_val_split()
 
-        # split samples into training versus validation
-        X_train, X_val, _, _ = train_test_split(self.df.index.values, 
-                                                        self.df.label.values, 
-                                                        test_size=0.15, 
-                                                        random_state=42, 
-                                                        stratify=self.df.label.values)
-        # label data type in column
-        self.df['data_type'] = ['not_set']*self.df.shape[0] 
-        self.df.loc[X_train, 'data_type'] = 'train'
-        self.df.loc[X_val, 'data_type'] = 'val'
-
-        return self.df
+        self.tokenize()
+        self.generate_tensors()
+        
 
 if __name__ == '__main__':
     
-    import torch
-    from transformers import BertTokenizer
-    from torch.utils.data import TensorDataset
-
     dm = DataManager()
-    df = dm.load_data(row_limit=500)
+    dm.load_data(row_limit=500)
 
-    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', 
-                                          do_lower_case=True)
-
-    encoded_data_train = tokenizer.batch_encode_plus(
-        df[df.data_type=='train'].text.values, 
-        add_special_tokens=True, 
-        return_attention_mask=True, 
-        pad_to_max_length=True, 
-        max_length=256, 
-        return_tensors='pt'
-    )
-
-    encoded_data_val = tokenizer.batch_encode_plus(
-        df[df.data_type=='val'].text.values, 
-        add_special_tokens=True, 
-        return_attention_mask=True, 
-        pad_to_max_length=True, 
-        max_length=256, 
-        return_tensors='pt'
-    )
-
-    input_ids_train = encoded_data_train['input_ids']
-    attention_masks_train = encoded_data_train['attention_mask']
-    labels_train = torch.tensor(df[df.data_type=='train'].label.values)
-
-    input_ids_val = encoded_data_val['input_ids']
-    attention_masks_val = encoded_data_val['attention_mask']
-    labels_val = torch.tensor(df[df.data_type=='val'].label.values)
-
-    dataset_train = TensorDataset(input_ids_train, attention_masks_train, labels_train)
-    dataset_val = TensorDataset(input_ids_val, attention_masks_val, labels_val)

a12eed68f2602a23bddf85b2cc995d0b5fbb9d9b
yishairasowsky <yishairasowsky@gmail.com>
2021-06-24 14:25:29 +0300

    remove unwanted load frunction set sample limitation for testing purposes

--- code/scripts/kaggle_ner.py
+++ code/scripts/kaggle_ner.py
@@ -1,7 +1,6 @@
 """
-*************
 kaggle_ner.py
-*************
+
 The purpose of this Python module is 
 to take data downloaded from one of the categories at
 https://www.kaggle.com/alaakhaled/conll003-englishversion,
@@ -9,18 +8,6 @@ and eventually output a shuffled dataset required for BERT classification.
 """
 import os
 import pandas as pd
-
-def load_data(type='train',random_state=42):
-    """
-    Returns a dataframe containing Kaggle's NER labeled entities.
-    Args:
-    - type (str) --> which portion of data, e.g. train/test/valid (default 'train')
-    - random_state (int) --> seed for repeating experiments (default 42)
-    """
-    file_name = f'data\\kaggle_ner\\{type}.txt'
-    raw_df = pd.read_csv(file_name,sep=' ',header=None)
-    return raw_df
-
 
 def process_sentences(filepath):
     final = []
@@ -37,12 +24,14 @@ def process_sentences(filepath):
     return final
 
 def load_data():
-    # base_path = '../input/conll003-englishversion/'
+
     base_path = os.path.join('data','kaggle_ner')
 
-    train_samples = process_sentences(os.path.join(base_path,'train.txt'))
-    test_samples = process_sentences(os.path.join(base_path,'test.txt'))
-    valid_samples = process_sentences(os.path.join(base_path,'valid.txt'))
+    sample_limit = 100
+
+    train_samples = process_sentences(os.path.join(base_path,'train.txt'))[:sample_limit]
+    test_samples = process_sentences(os.path.join(base_path,'test.txt'))[:sample_limit]
+    valid_samples = process_sentences(os.path.join(base_path,'valid.txt'))[:sample_limit]
 
     samples = train_samples + test_samples
 
@@ -52,6 +41,7 @@ def load_data():
     return train_samples,test_samples,valid_samples,samples,schema
 
 if __name__ == '__main__':
+
     train_samples,test_samples,valid_samples,samples,schema = load_data()
     print('done')
     # print(df.head())

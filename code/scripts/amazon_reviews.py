7008f88d09074257cdff5e4850f2a78cc7049982
yishairasowsky <yishairasowsky@gmail.com>
2021-06-24 17:50:02 +0000

    add all bert code

--- code/scripts/amazon_reviews.py
+++ code/scripts/amazon_reviews.py
@@ -9,11 +9,14 @@ The code is based upon
 https://github.com/naveenjafer/BERT_Amazon_Reviews/blob/master/main.py
 """
 import os
-import pandas as pd
 import torch
+import numpy as np
+import random
+import pandas as pd
 
-from transformers import BertTokenizer
-from torch.utils.data import TensorDataset
+from transformers import BertTokenizer, AdamW, get_linear_schedule_with_warmup
+from sklearn.metrics import f1_score
+from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
 from sklearn.model_selection import train_test_split
 
 class DataManager:
@@ -41,8 +44,8 @@ class DataManager:
     def set_target(self):
         # organize target variable
         possible_labels = self.df.overall.unique()
-        label_dict = {possible_label:possible_label - 1 for possible_label in possible_labels}
-        self.df['label'] = self.df.overall.replace(label_dict)
+        self.label_dict = {possible_label:possible_label - 1 for possible_label in possible_labels}
+        self.df['label'] = self.df.overall.replace(self.label_dict)
     
     def read_raw(self,product_category):
         # all raw data
@@ -120,9 +123,8 @@ class DataManager:
         attention_masks_val = self.encoded_data_val['attention_mask']
         labels_val = torch.tensor(self.df[self.df.data_type=='val'].label.values)
 
-        
-        dataset_train = TensorDataset(input_ids_train, attention_masks_train, labels_train)
-        dataset_val = TensorDataset(input_ids_val, attention_masks_val, labels_val)
+        self.dataset_train = TensorDataset(input_ids_train, attention_masks_train, labels_train)
+        self.dataset_val = TensorDataset(input_ids_val, attention_masks_val, labels_val)
 
     def load_data(self,row_limit=None,product_category='Appliances'):
         """
@@ -154,8 +156,143 @@ class DataManager:
         self.generate_tensors()
         
 
+def f1_score_func(preds, labels):
+    preds_flat = np.argmax(preds, axis=1).flatten()
+    labels_flat = labels.flatten()
+    return f1_score(labels_flat, preds_flat, average='weighted')
+
+def accuracy_per_class(preds, labels):
+    label_dict_inverse = {v: k for k, v in label_dict.items()}
+    
+    preds_flat = np.argmax(preds, axis=1).flatten()
+    labels_flat = labels.flatten()
+
+    for label in np.unique(labels_flat):
+        y_preds = preds_flat[labels_flat==label]
+        y_true = labels_flat[labels_flat==label]
+        print(f'Class: {label_dict_inverse[label]}')
+        print(f'Accuracy: {len(y_preds[y_preds==label])}/{len(y_true)}\n')
+
+def evaluate(dataloader_val):
+
+    model.eval()
+    
+    loss_val_total = 0
+    predictions, true_vals = [], []
+    
+    for batch in dataloader_val:
+        
+        batch = tuple(b.to(device) for b in batch)
+        
+        inputs = {'input_ids':      batch[0],
+                  'attention_mask': batch[1],
+                  'labels':         batch[2],
+                 }
+
+        with torch.no_grad():        
+            outputs = model(**inputs)
+            
+        loss = outputs[0]
+        logits = outputs[1]
+        loss_val_total += loss.item()
+
+        logits = logits.detach().cpu().numpy()
+        label_ids = inputs['labels'].cpu().numpy()
+        predictions.append(logits)
+        true_vals.append(label_ids)
+    
+    loss_val_avg = loss_val_total/len(dataloader_val) 
+    
+    predictions = np.concatenate(predictions, axis=0)
+    true_vals = np.concatenate(true_vals, axis=0)
+            
+    return loss_val_avg, predictions, true_vals
+    
 if __name__ == '__main__':
     
     dm = DataManager()
     dm.load_data(row_limit=500)
 
+    model = BertForSequenceClassification.from_pretrained("bert-base-uncased",
+                                                        num_labels=len(dm.label_dict),
+                                                        output_attentions=False,
+                                                        output_hidden_states=False)
+
+    batch_size = 3
+
+    dataloader_train = DataLoader(dm.dataset_train, 
+                                sampler=RandomSampler(dm.dataset_train), 
+                                batch_size=batch_size)
+
+    dataloader_validation = DataLoader(dm.dataset_val, 
+                                    sampler=SequentialSampler(dm.dataset_val), 
+                                    batch_size=batch_size)
+
+
+    optimizer = AdamW(model.parameters(),
+                    lr=1e-5, 
+                    eps=1e-8)
+
+    epochs = 5
+
+    scheduler = get_linear_schedule_with_warmup(optimizer, 
+                                                num_warmup_steps=0,
+                                                num_training_steps=len(dataloader_train)*epochs)
+
+    seed_val = 17
+    random.seed(seed_val)
+    np.random.seed(seed_val)
+    torch.manual_seed(seed_val)
+    torch.cuda.manual_seed_all(seed_val)
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model.to(device)
+
+    print(device)
+for epoch in tqdm(range(1, epochs+1)):
+    
+    model.train()
+    
+    loss_train_total = 0
+
+    progress_bar = tqdm(dataloader_train, desc='Epoch {:1d}'.format(epoch), leave=False, disable=False)
+    for batch in progress_bar:
+
+        model.zero_grad()
+        
+        batch = tuple(b.to(device) for b in batch)
+        
+        inputs = {'input_ids':      batch[0],
+                  'attention_mask': batch[1],
+                  'labels':         batch[2],
+                 }       
+
+        outputs = model(**inputs)
+        
+        loss = outputs[0]
+        loss_train_total += loss.item()
+        loss.backward()
+
+        torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+
+        optimizer.step()
+        scheduler.step()
+        
+        progress_bar.set_postfix({'training_loss': '{:.3f}'.format(loss.item()/len(batch))})
+         
+        
+    # torch.save(model.state_dict(), f'data_volume/finetuned_BERT_epoch_{epoch}.model')
+    torch.save(model.state_dict(), f'finetuned_BERT_epoch_{epoch}.model')
+        
+    tqdm.write(f'\nEpoch {epoch}')
+    
+    loss_train_avg = loss_train_total/len(dataloader_train)            
+    tqdm.write(f'Training loss: {loss_train_avg}')
+    
+    val_loss, predictions, true_vals = evaluate(dataloader_validation)
+    val_f1 = f1_score_func(predictions, true_vals)
+    tqdm.write(f'Validation loss: {val_loss}')
+    tqdm.write(f'F1 Score (Weighted): {val_f1}')
+    _, predictions, true_vals = evaluate(dataloader_validation)
+    results = accuracy_per_class(predictions, true_vals)
+    print(results)
